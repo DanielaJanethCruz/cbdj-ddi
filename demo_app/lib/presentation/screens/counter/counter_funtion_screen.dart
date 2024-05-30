182e193b1a5e4d95073c8b6d1d62b78f13c007aa
DanielaJanethCruz <daniela122cruz.11@gmail.com>
2024-05-30 16:41:29 -0600

    cambios

--- demo_app/lib/presentation/screens/counter/counter_funtion_screen.dart
+++ demo_app/lib/presentation/screens/counter/counter_funtion_screen.dart
@@ -14,27 +14,67 @@ class _counterFunctionScreenState extends State<counterFunctionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('$clickCounter',
-              style:
-                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-          Text(
-            '${clickCounter == 1 ? 'clic' : 'clics'}',
-            style: const TextStyle(fontSize: 25),
-          )
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          clickCounter++;
-          setState(() {});
-          ;
-        },
-        child: const Icon(Icons.exposure_plus_1),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Counter Functions"),
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          // backgroundColor: Colors.greenAccent,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.person_2_rounded),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.menu_open),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$clickCounter',
+                style: const TextStyle(
+                    fontSize: 160, fontWeight: FontWeight.w100)),
+            Text(
+              '${clickCounter == 1 ? 'clic' : 'clics'}',
+              style: const TextStyle(fontSize: 25),
+            )
+          ],
+        )),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                clickCounter=0;
+                setState(() {});
+                ;
+              },
+              child: const Icon(Icons.refresh_outlined),
+            ),
+            const SizedBox(height: 10,),
+            FloatingActionButton(
+              onPressed: () {
+                clickCounter--;
+                setState(() {});
+                ;
+              },
+              child: const Icon(Icons.exposure_minus_1),
+            ),
+            const SizedBox(height: 10,),
+            FloatingActionButton(
+              onPressed: () {
+                clickCounter++;
+                setState(() {});
+                ;
+              },
+              child: const Icon(Icons.exposure_plus_1),
+            ),
+          ],
+        ));
   }
 }
